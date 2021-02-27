c0bb0cf02167fbad812bdd41be7dce5f1d54d377
Asta Kat <73881655+astakat@users.noreply.github.com>
2021-02-27 16:56:27 +0100

    eerste JavaScript

---
+++ speeltuin/script.js
@@ -0,0 +1,16 @@
+console.log("Hello Winc Academy")
+let name = 'Asta';
+console.log(name);
+console.log(4 + 4);
+console.log("4"+"4");
+console.log(30 - 88);
+// let if = "bobo";
+console.log(7 % 5);
+let weather = 'sunny';
+console.log(weather);
+console.log(4*4);
+console.log(99/3);
+let age = 44;
+console.log(typeof age);
+age = 'vierenveertig';
+console.log(typeof age);
