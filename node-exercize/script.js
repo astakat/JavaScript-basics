6449b2a551cb9238ebe8c42849fcd945f338e8af
Asta Kat <73881655+astakat@users.noreply.github.com>
2021-03-01 10:52:33 +0100

    Add files via upload
    exercise; running node in console

---
+++ node-exercize/script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten")
+// dit is een grote som (je ziet dit niet omdat deze regel buiten het programma blijft..)
+let calc = 1230941 * 1823794;
+console.log(calc);
+let divi = 637263 / 54;
+console.log(divi);
+// regels hierboven gecomment en geuncomment :))
