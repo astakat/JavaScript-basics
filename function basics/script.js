2a36a1cedf0ec616aedf7e47db4b614aee0b4a5b
Asta Kat <73881655+astakat@users.noreply.github.com>
2021-03-03 10:27:31 +0100

    Add files via upload
    basic functions with pizza and sushi

---
+++ function basics/script.js
@@ -0,0 +1,20 @@
+const pizzaRecipe = function(){
+    console.log("make pizza dough");
+    console.log("put on desired topping");
+    console.log("bake pizza in the oven");
+    console.log("eat your pizza when it's hot");
+}
+// only running the code will not show you anything yet, you have to call or use the function too.
+pizzaRecipe();
+pizzaRecipe();
+
+const sushiMaking = function(){
+    console.log("make rice");
+    console.log("combine rice with avocado");
+    console.log("make a sushi roll");
+    console.log("slice and eat");
+}
+
+sushiMaking();
+sushiMaking();
+sushiMaking();
