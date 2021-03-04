c0700cf1ac0eb7eda9ff41bfd2006379769ff4a7
Asta Kat <73881655+astakat@users.noreply.github.com>
2021-03-04 13:37:56 +0100

    Add files via upload

---
+++ function-writing/script.js
@@ -0,0 +1,17 @@
+function multiply(number){
+    return number * number;
+}
+const result = multiply(7);
+console.log(result);
+
+const multiply2 = function(number){
+    return number * number;
+}
+const result2 = multiply2(8);
+console.log(result2);
+
+const multiply3 = (number) => {
+    return number * number;
+}
+const result3 = multiply3(9);
+console.log(result3);
